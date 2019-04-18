e4d6f24b1344d0fcbecfb1afbfe42d0e067c9dc6
i060847 <shachar.soel@sap.com>
2019-04-18 16:02:49 +0300

    - Separate the "tsc" compilcation of the generated files to the 'test' phase. - Added mocha tests to the 'test' phase.

--- packages/types/scripts/gen-dts.js
+++ packages/types/scripts/gen-dts.js
@@ -1,12 +1,12 @@
 const { jsonToDTS } = require("@ui5/dts-generator");
-const fs = require("fs-extra");
+const { emptyDirSync, readJsonSync, writeFileSync } = require("fs-extra");
 const { resolve, basename } = require("path");
 const childProcess = require("child_process");
 const klawSync = require("klaw-sync");
 const _ = require("lodash");
 
 const outputDir = resolve(__dirname, "../types");
-fs.emptyDirSync(outputDir);
+emptyDirSync(outputDir);
 
 const samplesDir = resolve(__dirname, "../input");
 const inputPaths = _.map(
@@ -17,7 +17,7 @@ const inputPaths = _.map(
 const inputJsons = _.map(inputPaths, path => {
   const fileName = basename(path);
   const libraryName = /^(.+)\.designtime\.api\.json$/.exec(fileName)[1];
-  const data = fs.readJsonSync(path);
+  const data = readJsonSync(path);
   // Generating The api.jsons using to ui5-cli seems to lose the `library` property
   data.library = libraryName;
   return data;
@@ -27,21 +27,9 @@ const inputJsons = _.map(inputPaths, path => {
 const dtsResults = jsonToDTS(inputJsons);
 
 _.forEach(dtsResults, dtsResult => {
-  fs.writeFileSync(
+  writeFileSync(
     resolve(outputDir, dtsResult.library + ".d.ts"),
     dtsResult.dtsText,
     "UTF8"
   );
-});
-
-// TODO: need to create index.d.ts ?
-
-childProcess.execSync(`npx tsc -init`, {
-  cwd: outputDir,
-  stdio: "inherit"
-});
-
-childProcess.execSync(`npx tsc`, {
-  cwd: outputDir,
-  stdio: "inherit"
 });
