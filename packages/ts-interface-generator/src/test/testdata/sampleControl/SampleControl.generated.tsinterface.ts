664b1ee07a32cd5286d5bd9d3c9c88eb9a605e1b
Andreas Kunz <andreas.kunz@sap.com>
2021-11-16 13:10:28 +0100

    chore(ts-interface-generator): add generated interface to test resources

---
+++ packages/ts-interface-generator/src/test/testdata/sampleControl/SampleControl.generated.tsinterface.ts
@@ -0,0 +1,66 @@
+import { CSSColor } from "sap/ui/core/library";
+import Control from "sap/ui/core/Control";
+import { AggregationBindingInfo } from "sap/ui/base/ManagedObject";
+import Event from "sap/ui/base/Event";
+import { $ButtonSettings } from "sap/m/Button";
+
+declare module "./SampleControl" {
+  /**
+   * Interface defining the settings object used in constructor calls
+   */
+  interface $SampleControlSettings extends $ButtonSettings {
+    subtext?: string;
+    textColor?: CSSColor;
+    content?: Control;
+    header?: Control;
+    partnerControl?: SampleControl | string;
+    alsoLabelledBy?: Control | string;
+    doublePress?: (event: Event) => void;
+  }
+
+  interface SampleControl {
+    // property: subtext
+    getSubtext(): string;
+    setSubtext(subtext: string): this;
+
+    // property: textColor
+    getTextColor(): CSSColor;
+    setTextColor(textColor: CSSColor): this;
+
+    // aggregation: content
+    getContent(): Control[];
+    addContent(content: Control): this;
+    insertContent(content: Control, index: number): this;
+    removeContent(content: number | string | Control): this;
+    removeAllContent(): Control[];
+    indexOfContent(content: Control): number;
+    destroyContent(): this;
+    bindContent(bindingInfo: AggregationBindingInfo): this;
+    unbindContent(): this;
+
+    // aggregation: header
+    getHeader(): Control;
+    setHeader(header: Control): this;
+    destroyHeader(): this;
+
+    // association: partnerControl
+    getPartnerControl(): string;
+    setPartnerControl(partnerControl?: string | SampleControl): this;
+
+    // association: alsoLabelledBy
+    getAlsoLabelledBy(): string[];
+    addAlsoLabelledBy(alsoLabelledBy: string | Control): this;
+    removeAlsoLabelledBy(alsoLabelledBy: number | string | Control): string;
+    removeAllAlsoLabelledBy(): string[];
+
+    // event: doublePress
+    attachDoublePress(fn: (event: Event) => void, listener?: object): this;
+    attachDoublePress<CustomDataType extends object>(
+      data: CustomDataType,
+      fn: (event: Event, data: CustomDataType) => void,
+      listener?: object
+    ): this;
+    detachDoublePress(fn: (event: Event) => void, listener?: object): this;
+    fireDoublePress(parameters?: object): this;
+  }
+}
