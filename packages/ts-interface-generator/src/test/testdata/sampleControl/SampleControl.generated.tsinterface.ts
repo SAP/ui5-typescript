812308e9d74ddc3cc179d3cc0c394630e1e102f6
Andreas Kunz <andreas.kunz@sap.com>
2021-12-22 16:27:01 +0100

    refactor(ts-interface-generator): prettier-ignore the generated control interface

--- packages/ts-interface-generator/src/test/testdata/sampleControl/SampleControl.generated.tsinterface.ts
+++ packages/ts-interface-generator/src/test/testdata/sampleControl/SampleControl.generated.tsinterface.ts
@@ -5,111 +5,106 @@ import Event from "sap/ui/base/Event";
 import { $ButtonSettings } from "sap/m/Button";
 
 declare module "./SampleControl" {
-  /**
-   * Interface defining the settings object used in constructor calls
-   */
-  interface $SampleControlSettings extends $ButtonSettings {
-    subtext?: string;
-    textColor?: CSSColor;
-    content?: Control[] | Control;
-    header?: Control;
-    partnerControl?: SampleControl | string;
-    alsoLabelledBy?: Control | string | (Control | string)[];
-    doublePress?: (event: Event) => void;
-  }
 
-  export default interface SampleControl {
-    // property: subtext
-    getSubtext(): string;
-    setSubtext(subtext: string): this;
+    /**
+     * Interface defining the settings object used in constructor calls
+     */
+    interface $SampleControlSettings extends $ButtonSettings {
+        subtext?: string;
+        textColor?: CSSColor;
+        content?: Control[] | Control;
+        header?: Control;
+        partnerControl?: SampleControl | string;
+        alsoLabelledBy?: Control | string | (Control | string)[];
+        doublePress?: (event: Event) => void;
+    }
 
-    // property: textColor
-    getTextColor(): CSSColor;
-    setTextColor(textColor: CSSColor): this;
+    export default interface SampleControl {
 
-    // aggregation: content
-    getContent(): Control[];
-    addContent(content: Control): this;
-    insertContent(content: Control, index: number): this;
-    removeContent(content: number | string | Control): this;
-    removeAllContent(): Control[];
-    indexOfContent(content: Control): number;
-    destroyContent(): this;
-    bindContent(bindingInfo: AggregationBindingInfo): this;
-    unbindContent(): this;
+        // property: subtext
+        getSubtext(): string;
+        setSubtext(subtext: string): this;
 
-    // aggregation: header
-    getHeader(): Control;
-    setHeader(header: Control): this;
-    destroyHeader(): this;
+        // property: textColor
+        getTextColor(): CSSColor;
+        setTextColor(textColor: CSSColor): this;
 
-    // association: partnerControl
-    getPartnerControl(): string;
-    setPartnerControl(partnerControl?: string | SampleControl): this;
+        // aggregation: content
+        getContent(): Control[];
+        addContent(content: Control): this;
+        insertContent(content: Control, index: number): this;
+        removeContent(content: number | string | Control): this;
+        removeAllContent(): Control[];
+        indexOfContent(content: Control): number;
+        destroyContent(): this;
+        bindContent(bindingInfo: AggregationBindingInfo): this;
+        unbindContent(): this;
 
-    // association: alsoLabelledBy
-    getAlsoLabelledBy(): string[];
-    addAlsoLabelledBy(alsoLabelledBy: string | Control): this;
-    removeAlsoLabelledBy(alsoLabelledBy: number | string | Control): string;
-    removeAllAlsoLabelledBy(): string[];
+        // aggregation: header
+        getHeader(): Control;
+        setHeader(header: Control): this;
+        destroyHeader(): this;
 
-    // event: doublePress
-    attachDoublePress(fn: (event: Event) => void, listener?: object): this;
-    attachDoublePress<CustomDataType extends object>(
-      data: CustomDataType,
-      fn: (event: Event, data: CustomDataType) => void,
-      listener?: object
-    ): this;
-    detachDoublePress(fn: (event: Event) => void, listener?: object): this;
-    fireDoublePress(parameters?: object): this;
-  }
+        // association: partnerControl
+        getPartnerControl(): string;
+        setPartnerControl(partnerControl?: string | SampleControl): this;
+
+        // association: alsoLabelledBy
+        getAlsoLabelledBy(): string[];
+        addAlsoLabelledBy(alsoLabelledBy: string | Control): this;
+        removeAlsoLabelledBy(alsoLabelledBy: number | string | Control): string;
+        removeAllAlsoLabelledBy(): string[];
+
+        // event: doublePress
+        attachDoublePress(fn: (event: Event) => void, listener?: object): this;
+        attachDoublePress<CustomDataType extends object>(data: CustomDataType, fn: (event: Event, data: CustomDataType) => void, listener?: object): this;
+        detachDoublePress(fn: (event: Event) => void, listener?: object): this;
+        fireDoublePress(parameters?: object): this;
+    }
 }
 
 // this duplicate interface without export is needed to avoid "Cannot find name 'SampleControl'" TypeScript errors above
 declare module "./SampleControl" {
-  interface SampleControl {
-    // property: subtext
-    getSubtext(): string;
-    setSubtext(subtext: string): this;
+    interface SampleControl {
 
-    // property: textColor
-    getTextColor(): CSSColor;
-    setTextColor(textColor: CSSColor): this;
+        // property: subtext
+        getSubtext(): string;
+        setSubtext(subtext: string): this;
 
-    // aggregation: content
-    getContent(): Control[];
-    addContent(content: Control): this;
-    insertContent(content: Control, index: number): this;
-    removeContent(content: number | string | Control): this;
-    removeAllContent(): Control[];
-    indexOfContent(content: Control): number;
-    destroyContent(): this;
-    bindContent(bindingInfo: AggregationBindingInfo): this;
-    unbindContent(): this;
+        // property: textColor
+        getTextColor(): CSSColor;
+        setTextColor(textColor: CSSColor): this;
 
-    // aggregation: header
-    getHeader(): Control;
-    setHeader(header: Control): this;
-    destroyHeader(): this;
+        // aggregation: content
+        getContent(): Control[];
+        addContent(content: Control): this;
+        insertContent(content: Control, index: number): this;
+        removeContent(content: number | string | Control): this;
+        removeAllContent(): Control[];
+        indexOfContent(content: Control): number;
+        destroyContent(): this;
+        bindContent(bindingInfo: AggregationBindingInfo): this;
+        unbindContent(): this;
 
-    // association: partnerControl
-    getPartnerControl(): string;
-    setPartnerControl(partnerControl?: string | SampleControl): this;
+        // aggregation: header
+        getHeader(): Control;
+        setHeader(header: Control): this;
+        destroyHeader(): this;
 
-    // association: alsoLabelledBy
-    getAlsoLabelledBy(): string[];
-    addAlsoLabelledBy(alsoLabelledBy: string | Control): this;
-    removeAlsoLabelledBy(alsoLabelledBy: number | string | Control): string;
-    removeAllAlsoLabelledBy(): string[];
+        // association: partnerControl
+        getPartnerControl(): string;
+        setPartnerControl(partnerControl?: string | SampleControl): this;
 
-    // event: doublePress
-    attachDoublePress(fn: (event: Event) => void, listener?: object): this;
-    attachDoublePress<CustomDataType extends object>(
-      data: CustomDataType,
-      fn: (event: Event, data: CustomDataType) => void,
-      listener?: object
-    ): this;
-    detachDoublePress(fn: (event: Event) => void, listener?: object): this;
-    fireDoublePress(parameters?: object): this;
-  }
+        // association: alsoLabelledBy
+        getAlsoLabelledBy(): string[];
+        addAlsoLabelledBy(alsoLabelledBy: string | Control): this;
+        removeAlsoLabelledBy(alsoLabelledBy: number | string | Control): string;
+        removeAllAlsoLabelledBy(): string[];
+
+        // event: doublePress
+        attachDoublePress(fn: (event: Event) => void, listener?: object): this;
+        attachDoublePress<CustomDataType extends object>(data: CustomDataType, fn: (event: Event, data: CustomDataType) => void, listener?: object): this;
+        detachDoublePress(fn: (event: Event) => void, listener?: object): this;
+        fireDoublePress(parameters?: object): this;
+    }
 }
