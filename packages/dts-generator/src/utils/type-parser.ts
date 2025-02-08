4d170e2e16bb330917ce01cf33e211d5bf770b52
Frank Weigel <frank.weigel@sap.com>
2025-02-08 09:01:03 +0100

    Revert "Support constructor types and option parameters in function types (#485)"
    This reverts commit 77f8226b7b2f1837a391e2061157148f77df19c6.

--- packages/dts-generator/src/utils/type-parser.ts
+++ packages/dts-generator/src/utils/type-parser.ts
@@ -134,12 +134,6 @@ export function TypeParser(
         next(":");
         returnType = parseType();
       }
-      if (constructorType != null && returnType != null) {
-        throw new SyntaxError(
-          `A function signature must either use the 'new' keyword or have a return type, ` +
-            `but not both (pos: ${rLexer.lastIndex}, input='${input}')`,
-        );
-      }
       type = builder.function(
         paramTypes,
         returnType,
